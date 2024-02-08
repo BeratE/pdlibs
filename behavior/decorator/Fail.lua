6e4d21d44b0c07375c5aa618aa6c901efc3ba214
bertural <b.ertural@seatback.de>
2024-02-08 21:14:37 +0100

    Add Fail decorator behavior

---
+++ behavior/decorator/Fail.lua
@@ -0,0 +1,22 @@
+import "CoreLibs/object"
+
+import "pdlibs/behavior/decorator/Decorator"
+import "pdlibs/behavior/leaf/Action"
+
+-- Basic Succeeder. Always do action and return success.
+class('Fail', {}, mylib.behavior).extends(mylib.behavior.Decorator)
+
+function mylib.behavior.Fail:init(child)
+    if (type(child) == "nil") then
+        child = mylib.behavior.Action()
+    end
+    mylib.behavior.Fail.super.init(self, child)
+end
+
+function mylib.behavior.Fail:onUpdate()
+    local status = self.child:update()
+    if (status == mylib.behavior.Status.RUNNING) then
+        return status
+    end
+    return mylib.behavior.Status.FAILURE
+end
