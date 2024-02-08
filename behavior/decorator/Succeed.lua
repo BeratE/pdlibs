8293995c2b1f9df5044c4577ff1feb77d8dd6915
bertural <b.ertural@seatback.de>
2024-02-08 21:09:28 +0100

    Renamo Do decorator to Succeed

--- behavior/decorator/Succeed.lua
+++ behavior/decorator/Succeed.lua
@@ -4,16 +4,16 @@ import "pdlibs/behavior/decorator/Decorator"
 import "pdlibs/behavior/leaf/Action"
 
 -- Basic Succeeder. Always do action and return success.
-class('Do', {}, mylib.behavior).extends(mylib.behavior.Decorator)
+class('Succeed', {}, mylib.behavior).extends(mylib.behavior.Decorator)
 
-function mylib.behavior.Do:init(child)
+function mylib.behavior.Succeed:init(child)
     if (type(child) == "nil") then
         child = mylib.behavior.Action()
     end
-    mylib.behavior.Do.super.init(self, child)
+    mylib.behavior.Succeed.super.init(self, child)
 end
 
-function mylib.behavior.Do:onUpdate()
+function mylib.behavior.Succeed:onUpdate()
     local status = self.child:update()
     if (status == mylib.behavior.Status.RUNNING) then
         return status
