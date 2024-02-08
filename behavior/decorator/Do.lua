f47a47aa976e0c435abeb2782c9905147f534cd8
bertural <b.ertural@seatback.de>
2024-02-08 19:34:06 +0100

    Allow empty Do node

--- behavior/decorator/Do.lua
+++ behavior/decorator/Do.lua
@@ -1,11 +1,15 @@
 import "CoreLibs/object"
 
 import "pdlibs/behavior/decorator/Decorator"
+import "pdlibs/behavior/leaf/Action"
 
 -- Basic Succeeder. Always do action and return success.
 class('Do', {}, mylib.behavior).extends(mylib.behavior.Decorator)
 
 function mylib.behavior.Do:init(child)
+    if (type(child) == "nil") then
+        child = mylib.behavior.Action()
+    end
     mylib.behavior.Do.super.init(self, child)
 end
 
