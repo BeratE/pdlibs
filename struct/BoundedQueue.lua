73f83673752ab0daff02107910949ccb7bb4740c
bertural <b.ertural@seatback.de>
2024-02-09 15:14:39 +0100

    Fallback BoundedQueue to standard queue if bound is nil

--- struct/BoundedQueue.lua
+++ struct/BoundedQueue.lua
@@ -5,15 +5,22 @@ import "pdlibs/struct/Queue"
 -- A BoundedQueue stores a maximum number of bound elements
 class('BoundedQueue', {}, mylib.struct).extends(mylib.struct.Queue)
 
-function mylib.struct.BoundedQueue:init(bound)
-    mylib.struct.BoundedQueue.super.init(self)
+function mylib.struct.BoundedQueue:init(bound, list)
+    mylib.struct.BoundedQueue.super.init(self, list)
+    -- Init bound
     self.bound = bound
+    self.boundCheck = function() end
+    if (bound ~= nil) then
+        self.boundCheck = function ()
+            if (self:size() >= self.bound) then
+                self:pop()
+            end
+        end
+    end
 end
 
 -- Push item to back of queue and pop last item if bound is reached
 function mylib.struct.BoundedQueue:push(item)
-    if (self:size() >= self.bound) then
-        self:pop()
-    end
+    self:boundCheck()
     mylib.struct.BoundedQueue.super.push(self, item)
 end
