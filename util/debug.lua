e4c8fdad884f0f26dd6f0fa19a81ff15d24197a2
bertural <b.ertural@seatback.de>
2024-02-12 14:16:43 +0100

    Minor changes to dump function

--- util/debug.lua
+++ util/debug.lua
@@ -5,10 +5,6 @@ mylib = mylib or {}
 -- Dump table contents in json-like format
 function mylib.dump(o)
     if type(o) == 'table' then
-        -- Check if Object
-        if (type(o.isa) == "function") then
-            return ".."
-        end
         local s = '{ '
         for k, v in pairs(o) do
             if type(k) ~= 'number' then
